4132ecf5043231bbc5de14f4613746222ce3cc85
Mikael Hardø <mikael@hardo.dk>
2017-01-31 13:36:35 +0100

    first working version
    Still very rough and not very simple to use

---
+++ EconomyAnalyzer/Entitites/Row.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomyAnalyzer.Entitites
+{
+    public class Row
+    {
+
+        public Row(string s)
+        {
+            //Bogført;Tekst;Rentedato;Beløb;Saldo
+            //24-12-2017;Dankort-nota Føtex Malmø  45321;27-12-2017;-32,25;3669,16
+
+            if (s.StartsWith("Bog") || string.IsNullOrWhiteSpace(s))
+            {
+                this.Comment = string.Empty;
+
+                return;
+            }
+
+            var columns = s.Split(';');
+
+            this.Date = DateTime.ParseExact(columns[2], "dd-MM-yyyy", CultureInfo.InvariantCulture);
+            this.Comment = columns[1];
+            this.Amount = decimal.Parse(columns[3], CultureInfo.CreateSpecificCulture("da-DK"));
+            this.Balance = !string.IsNullOrWhiteSpace(columns[4]) ? decimal.Parse(columns[4], CultureInfo.CreateSpecificCulture("da-DK")) : 0;
+        }
+
+        public decimal Amount { get; private set; }
+        public decimal Balance { get; private set; }
+        public string Comment { get; private set; }
+        public string Category { get; set; }
+
+        public string SanitizedComment
+        {
+            get
+            {
+                // Comment Example 1:
+                // "Dankort-nota H&M 896         5834"
+
+                // Comment Example 2:
+                // "Visa køb DKK      97,00            VISTAPR*VistaPrint.d               Den 23.11"
+
+                // Comment Example 3:
+                // "Pengeautomat den 13.08. kl. 17.09  2344-45 eksnr. 2144"
+                // "Fr. automat den 20.08. kl. 12.37   3450 eksnr.     9704"
+
+                // Comment Example 4;
+                // "Bgs egen konto"
+
+                // Comment Example 5:
+                // "Bs betaling 11571532-00021"
+
+                // Comment Example 6:
+                // "Ovf. fra, konto nr. 5678-345-124"
+
+                // the real comment is always 14 chars long
+
+                var result = "";
+
+                if (string.IsNullOrWhiteSpace(this.Comment))
+                {
+                    result = string.Empty;
+                }
+
+                else if (this.Comment.StartsWith("Ovf."))
+                {
+                    result = this.Comment;
+                }
+
+                else if(this.Comment.StartsWith("Bs betaling"))
+                {
+                    result = $"Betalings Service: {this.Comment.Replace("Bs betaling ", "")}";
+                }
+
+                else if (this.Comment.StartsWith("Bgs"))
+                {
+                    result = $"Overførsel: {this.Comment.Replace("Bgs ", "")}";
+                }
+
+                else if (this.Comment.StartsWith("Dankort"))
+                {
+                    result = this.Comment.Replace("Dankort-nota ", "").Substring(0, 14);
+                }
+
+                else if (this.Comment.StartsWith("Pengeautomat") || this.Comment.StartsWith("Fr. automat"))
+                {
+                    result = "Pengeautomat";
+                }
+
+                else if (this.Comment.StartsWith("Visa"))
+                {
+                    result = this.Comment.Substring(35, 19);
+                }
+                else
+                {
+                    result = this.Comment;
+                }
+
+                return result.Replace(",", "-");
+            }
+        }
+
+        internal string ToString(string[] categories)
+        {
+            var result = this.ToString();
+
+            foreach (var category in categories)
+            {
+                if(this.Category == category)
+                {
+                    result += this.Amount * -1;
+                }
+
+                result += ",";
+            }
+
+
+            return result;
+        }
+
+        public DateTime Date { get; private set; }
+
+        public override string ToString()
+        {
+            if(this.Date == null)
+            {
+                return string.Empty;
+            }
+
+            return $"{Date.ToString("yyyy-MM-dd")},{SanitizedComment.Trim()},{Amount},{Balance},";
+        }
+
+    }
+}
---
+++ EconomyAnalyzer/Program.cs
@@ -0,0 +1,160 @@
+﻿using EconomyAnalyzer.Entitites;
+using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EconomyAnalyzer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read file (csv parser)
+            var path = @"c:\temp\";
+
+            var categories = File.ReadAllLines(path + "categories.txt", Encoding.Default).ToArray();
+            var i = 0;
+            foreach (var category in categories)
+            {
+                Console.WriteLine($"{i++}: {category}");
+            }
+            Console.WriteLine("---------------------------------------");
+
+            var links = File.ReadAllLines(path + "links.txt", Encoding.Default).Select(s => {
+                var split = s.Split('|');
+                return new { Key = split[0], Value = split[1] };
+            })
+            .ToDictionary(s => s.Key,s => s.Value);
+
+            var lines = File.ReadAllLines(path + "poster.csv", Encoding.Default);
+
+            // the first two lines are empty and the header
+            var rows = lines.Skip(2).Select(s => new Row(s)).Where(s => s != null).ToList();
+
+            UpdateRows(rows, categories, links);
+
+            // map lines to category
+            while (rows.Where(s => string.IsNullOrEmpty(s.Category)).Any())
+            {
+                var row = rows.Where(s => string.IsNullOrEmpty(s.Category)).First();
+
+                if (links.ContainsKey(row.SanitizedComment))
+                {
+                    row.Category = links[row.SanitizedComment];
+                    continue;
+                }
+                var count = rows.Count(s => string.IsNullOrEmpty(s.Category));
+                // hacks to clear the line
+                Console.Write($"\r({count}) Hvad er det: {row.SanitizedComment}                                     ");
+                var result = Console.ReadKey();
+                var index = int.Parse(result.KeyChar.ToString());
+
+                row.Category = categories[index];
+                links[row.SanitizedComment] = row.Category;
+
+                File.AppendAllLines(path + "links.txt", new List<string> { $"{row.SanitizedComment}|{row.Category}" });
+                UpdateRows(rows, categories, links);
+            }
+
+            rows = rows.OrderBy(s => s.Date).ThenByDescending(s => s.Balance).ToList();
+
+            // output to xsls file
+            var package = new ExcelPackage();
+            ExcelWorksheet currentSheet = null;
+            int currentRow = 2;
+            var sheetName = "";
+
+
+            foreach (var item in rows)
+            {
+                var newSheetName = $"{item.Date.ToString("MMMMM")} {item.Date.Year}";
+
+
+                if (sheetName != newSheetName)
+                {
+                    if(currentSheet != null)
+                    {
+                        for (int j = 0; j <= categories.Length; j++)
+                        {
+                            var index = j + 3;
+
+                            // Capital A starts at char value 65, so we start one before that
+                            var letter = Char.ConvertFromUtf32(index + 64);
+                            currentSheet.Cells[currentRow, index].Formula = $"=SUM({letter}2:{letter}{currentRow-1})";
+                        }
+
+                        // Make the totals bold
+                        using (var range = currentSheet.Cells[currentRow, 1, currentRow, 3 + categories.Length])
+                        {
+                            range.Style.Font.Bold = true;
+                        }
+
+
+                        // Select all the cells to adjust cell width
+                        using (var range = currentSheet.Cells["A1:L500"])
+                        {
+                            range.AutoFitColumns();
+                        }
+                    }
+
+                    sheetName = newSheetName;
+                    currentSheet = package.Workbook.Worksheets.Add(sheetName);
+
+                    // headers
+                    currentSheet.Cells[1, 1].Value = "Dato";
+                    currentSheet.Cells[1, 2].Value = "Text";
+                    currentSheet.Cells[1, 3].Value = "Ændring";
+
+                    var start = 4;
+                    foreach (var category in categories)
+                    {
+                        currentSheet.Cells[1, start++].Value = category;
+                    }
+
+                    currentRow = 2;
+                }
+
+                currentSheet.Cells[currentRow, 1].Value = item.Date;
+                currentSheet.Cells[currentRow, 1].Style.Numberformat.Format = "yyyy-mm-dd";
+                currentSheet.Cells[currentRow, 2].Value = item.SanitizedComment;
+                currentSheet.Cells[currentRow, 3].Value = item.Amount;
+
+                var categoryIndex = categories.ToList().IndexOf(item.Category) + 1;
+                currentSheet.Cells[currentRow, 3 + categoryIndex].Value = Math.Abs(item.Amount);
+
+                currentRow++;
+            }
+
+            File.WriteAllBytes(path + $"result {DateTime.Now.ToString("yyyy-MM-dd HHmm")}.xlsx", package.GetAsByteArray());
+
+            //// Add headers
+            //var content = new List<string>();
+            //content.Add("Dato,Tekst,Beløb,Saldo," + string.Join(",", categories));
+
+            //content.AddRange(rows.Select(s => s.ToString(categories)));
+
+
+            //var newFile = string.Join(Environment.NewLine, content);
+            //File.WriteAllText(path + "posterout.csv",  newFile, Encoding.Default);
+        }
+
+        private static void UpdateRows(List<Row> rows, string[] categories, Dictionary<string, string> links)
+        {
+            foreach (var row in rows)
+            {
+                if (!string.IsNullOrEmpty(row.Category))
+                {
+                    continue;
+                }
+
+                if (links.ContainsKey(row.SanitizedComment))
+                {
+                    row.Category = links[row.SanitizedComment];
+                }
+            }
+        }
+    }
+}
